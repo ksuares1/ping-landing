784067d1332f4d78d3b25f59c79310cf3785f734
Kimberly Suares <kimberlyjsuares@gmai.com>
2024-05-25 12:48:19 -0400

    added js

---
+++ column.js
@@ -0,0 +1,16 @@
+/*
+Pseudocode
+1. User to add their email
+2. If the user adds a correct email address, then we will say thank you
+3. Else if , add an error message that says "please fill in required field"
+4. If it left blank please fill out field. - It will them an error message
+*/
+
+
+function emailInput(input){
+    if (input === "email"){
+        console.log("Thank you!");
+    }
+    
+
+}
