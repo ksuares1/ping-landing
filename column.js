b68d3891d0a2ec89de03e3d19650905da66f4f31
Kimberly Suares <kimberlyjsuares@gmai.com>
2024-06-01 12:33:15 -0400

    added function to js

--- column.js
+++ column.js
@@ -48,4 +48,7 @@ Pseudocode
             }
           });
 
-        
+        function validateEmail(email) {
+          const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+          return re.test(String(email).toLowerCase());
+        }
